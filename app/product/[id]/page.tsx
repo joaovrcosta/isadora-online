8e382657c3e337c227cfabf201e11f84ff6f166a
joaovrcosta <joaovr.costa@gmail.com>
2024-12-02 12:20:09 -0300

    fix: more procuts carousel

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -8,11 +8,15 @@ import logoTargeta from '@/public/icons/logo-tarjeta.webp';
 import { Heart, Share2 } from 'lucide-react';
 import ProductImages from '@/components/product/product-images';
 import { useState } from 'react';
-import { formatCurrencyARS } from '@/lib/utils';
-import isaClubLogo from '@/public/icons/ISACLUB-logo-100.webp'
-import { ProductItem } from '@/components/product-item';
+import isaClubLogo from '@/public/icons/ISACLUB-logo-100.webp';
 import { CarouselComponent } from '@/components/show-more-products';
 import { mockProducts } from '@/app/_mocks/products';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/shadcn/ui/tooltip';
 
 const product = {
   sku: '48611201',
@@ -45,6 +49,10 @@ const product = {
     description:
       'Pagá con Visa, MasterCard, Mercado Pago y Naranja plan Z en 3 cuotas sin interés en compras superiores a $30.000 y 6 cuotas sin interés en compras superiores a $60.000.',
   },
+  colors: [
+    { value: 'green', label: 'Green', colorCode: '#88cc88' },
+    { value: 'brown', label: 'Brown', colorCode: '#a0522d' },
+  ],
 };
 
 export default function ProductPage() {
@@ -55,204 +63,225 @@ export default function ProductPage() {
   };
 
   return (
-   <div>
-     <div className="mt-[76px] flex flex-col items-center justify-center md:mt-24 lg:mt-12 lg:flex-row">
-      <div className="flex flex-col md:flex-row lg:flex-row">
-        <ProductImages images={product.images} />
+    <div>
+      <div className="mt-[76px] flex flex-col items-center justify-center md:mt-24 lg:mt-12 lg:flex-row">
+        <div className="flex flex-col md:flex-row lg:flex-row">
+          <ProductImages images={product.images} />
 
-        <section className="md:lg-8 ml-0 flex h-full max-w-[760px] flex-col justify-start px-4 md:w-[43%] lg:ml-4 lg:w-[43%]">
-          <div className="flex items-center justify-between">
-            <div className="mb-4 flex items-center gap-2">
-              <span className="text-[11px] tracking-[2px] text-gray-300">
-                SKU
-              </span>
-              <span className="text-[11px] tracking-[2px] text-gray-300">
-                {product.sku}
-              </span>
-            </div>
-            <div className="mb-4 flex items-center justify-center space-x-3 md:mb-0 lg:mb-0">
-              <div className="flex flex-col items-center justify-center">
-                <button className="hover:text-lightPink">
-                  <Heart
-                    size={32}
-                    className="h-[24px] w-[24px] lg:h-[32px] lg:w-[32px]"
-                    strokeWidth={0.8}
-                  />
-                </button>
+          <section className="md:lg-8 ml-0 flex h-full max-w-[760px] flex-col justify-start px-4 md:w-[43%] lg:ml-4 lg:w-[43%]">
+            <div className="flex items-center justify-between">
+              <div className="mb-4 flex items-center gap-2">
+                <span className="text-[11px] tracking-[2px] text-gray-300">
+                  SKU
+                </span>
+                <span className="text-[11px] tracking-[2px] text-gray-300">
+                  {product.sku}
+                </span>
               </div>
-              <div className="flex flex-col items-center justify-center">
-                <button className="duration-75 hover:text-lightPink">
-                  <Share2
-                    size={32}
-                    className="h-[24px] w-[24px] lg:h-[32px] lg:w-[32px]"
-                    strokeWidth={0.8}
-                  />
-                </button>
+              <div className="mb-4 flex items-center justify-center space-x-3 md:mb-0 lg:mb-0">
+                <div className="flex flex-col items-center justify-center">
+                  <button className="hover:text-lightPink">
+                    <Heart
+                      size={32}
+                      className="h-[24px] w-[24px] lg:h-[32px] lg:w-[32px]"
+                      strokeWidth={0.8}
+                    />
+                  </button>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <button className="duration-75 hover:text-lightPink">
+                    <Share2
+                      size={32}
+                      className="h-[24px] w-[24px] lg:h-[32px] lg:w-[32px]"
+                      strokeWidth={0.8}
+                    />
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
 
-          <h1 className="mb-4 text-[14px] font-normal tracking-[2px] text-gray-900 md:text-[22px] md:font-bold lg:text-[22px] lg:font-bold">
-            {product.name}
-          </h1>
-          <div className="md:max-w-[456px] mb-6 max-w-full border-b border-gray-200  text-lg text-gray-900 md:pb-10 lg:pb-10 lg:pl-0">
-  <div className="lg:flex md:flex xl:flex hidden items-center justify-start space-x-4">
-    {product.discount ? (
-      <>
-        <span className="line-through text-gray-500">$ {product.price}</span>
-        <span className="text-black font-medium">$ {product.discountPrice}</span>
-      </>
-    ) : (
-      <span>${product.price}</span>
-    )}
-    
-    {product.discount && (
-     <div className='bg-lightPink/30 px-3 py-1'>
-       <span className="text-black text-sm font-bold">
-        {product.discount}% OFF
-      </span>
-     </div>
-    )}
-  </div>
+            <h1 className="mb-4 text-[14px] font-normal tracking-[2px] text-gray-900 md:text-[22px] md:font-bold lg:text-[22px] lg:font-bold">
+              {product.name}
+            </h1>
+            <div className="mb-6 max-w-full border-b border-gray-200 text-lg text-gray-900 md:max-w-[456px] md:pb-10 lg:pb-10 lg:pl-0">
+              <div className="hidden items-center justify-start space-x-4 md:flex lg:flex xl:flex">
+                {product.discount ? (
+                  <>
+                    <span className="text-gray-500 line-through">
+                      $ {product.price}
+                    </span>
+                    <span className="font-medium text-black">
+                      $ {product.discountPrice}
+                    </span>
+                  </>
+                ) : (
+                  <span>${product.price}</span>
+                )}
 
-  <div className='bg-[#eeeeee]  mt-5 px-3 py-1 rounded-lg flex items-center justify-start w-full gap-3 mb-5'>
-  <Image src={isaClubLogo} alt="" height={13} width={63} className='h-[12px]' />
-  <span className='lg:text-[14px] xl:text-[14px] md:text-[14px] text-[12px] text-red-600'>Registrate y sumá puntos!</span>
-</div>
+                {product.discount && (
+                  <div className="bg-lightPink/30 px-3 py-1">
+                    <span className="text-sm font-bold text-black">
+                      {product.discount}% OFF
+                    </span>
+                  </div>
+                )}
+              </div>
 
-</div>
+              <div className="mb-5 mt-5 flex w-full items-center justify-start gap-3 rounded-lg bg-[#eeeeee] px-3 py-1">
+                <Image
+                  src={isaClubLogo}
+                  alt=""
+                  height={13}
+                  width={63}
+                  className="h-[12px]"
+                />
+                <span className="text-[12px] text-red-600 md:text-[14px] lg:text-[14px] xl:text-[14px]">
+                  Registrate y sumá puntos!
+                </span>
+              </div>
+            </div>
 
+            <div className="mb-6 space-y-2">
+              <div className="mb-6 space-y-3">
+                <h4 className="text-xs font-bold text-gray-800">DESCRIÇÃO</h4>
 
-      
+                <p className="text-xs text-gray-700">{product.description}</p>
+                {showMore && (
+                  <p className="text-xs text-gray-700">
+                    {product.extraDescription}
+                  </p>
+                )}
+              </div>
 
+              <button
+                onClick={toggleShowMore}
+                className="mt-6 text-xs text-black"
+              >
+                <span className="tracking-[2px] underline">
+                  {showMore ? 'Ver menos' : 'Ver más'}
+                </span>
+              </button>
+            </div>
 
-          <div className="mb-6 space-y-2">
-            <div className="mb-6 space-y-3">
-              <h4 className="text-xs font-bold text-gray-800">DESCRIÇÃO</h4>
+            <Button className="mb-10 mt-4 hidden h-[50px] w-[250px] rounded-none text-[16px] md:block lg:block">
+              ADICIONAR AO CARRINHO
+            </Button>
 
-              <p className="text-xs text-gray-700">{product.description}</p>
-              {showMore && (
-                <p className="text-xs text-gray-700">
-                  {product.extraDescription}
-                </p>
+            <div className="mb-10">
+              <span className="text-[11px] font-semibold tracking-[2px]">
+                MÁS OPCIONES DISPONIBLES:
+              </span>
+              <div className="mt-2 flex space-x-2">
+                {product.colors.map((color, key) => (
+                  <TooltipProvider key={key}>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <div
+                          key={color.value}
+                          className="h-[28px] w-[28px] cursor-pointer rounded-full"
+                          style={{ backgroundColor: color.colorCode }}
+                        ></div>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>{color.label}</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                ))}
+              </div>
+            </div>
+
+            <div className="space-y-6">
+              {product.shippingInfo && (
+                <div className="flex items-center space-x-4">
+                  <div className="min-w-[45px]">
+                    <Image
+                      src={envioImg}
+                      width={45}
+                      height={45}
+                      alt="Envio gratis"
+                    />
+                  </div>
+                  <div>
+                    <h4 className="text-[11px] font-semibold tracking-[2px] text-lightPink">
+                      {product.shippingInfo.title}
+                    </h4>
+                    <p className="text-[11px] leading-[20px] tracking-[2px] text-gray-600">
+                      {product.shippingInfo.description}
+                    </p>
+                  </div>
+                </div>
+              )}
+
+              {product.returnInfo && (
+                <div className="flex items-center space-x-4">
+                  <div className="min-w-[45px]">
+                    <Image
+                      src={cambiosImg}
+                      width={45}
+                      height={45}
+                      alt="Cambios y devoluciones"
+                    />
+                  </div>
+                  <div>
+                    <h4 className="text-[11px] font-semibold tracking-[2px] text-lightPink">
+                      {product.returnInfo.title}
+                    </h4>
+                    <p className="text-[11px] leading-[20px] tracking-[2px] text-gray-600">
+                      {product.returnInfo.description}
+                    </p>
+                  </div>
+                </div>
+              )}
+
+              {product.paymentInfo && (
+                <div className="flex items-center space-x-4">
+                  <div className="min-w-[45px]">
+                    <Image
+                      src={logoTargeta}
+                      width={45}
+                      height={45}
+                      alt="Cambios y devoluciones"
+                    />
+                  </div>
+                  <div>
+                    <h4 className="text-[11px] font-semibold tracking-[2px] text-lightPink">
+                      {product.paymentInfo.title}
+                    </h4>
+                    <p className="text-[11px] leading-[20px] tracking-[2px] text-gray-600">
+                      {product.paymentInfo.description}
+                    </p>
+                  </div>
+                </div>
               )}
             </div>
-            
-            <button
-              onClick={toggleShowMore}
-              className="mt-6 text-xs text-black"
-            >
-              <span className="tracking-[2px] underline">
-                {showMore ? 'Ver menos' : 'Ver más'}
+          </section>
+        </div>
+
+        <div className="fixed bottom-0 left-0 right-0 z-10 bg-white shadow-md md:hidden">
+          <div className="flex items-center justify-between px-4 py-2">
+            <div>
+              <span className="text-[14px] text-lg tracking-[2px] text-black">
+                ${product.price}
               </span>
-            </button>
-          </div>
-
-          
-
-          <Button className="mb-10 mt-4 hidden h-[50px] w-[250px] rounded-none text-[16px] md:block lg:block">
-            ADICIONAR AO CARRINHO
-          </Button>
-
-          <div className='mb-10'>
-            <span className='text-[11px] tracking-[2px] font-semibold'>MÁS OPCIONES DISPONIBLES:</span>
-            <div className='flex space-x-2 mt-2'>
-            <div className='h-[28px] w-[28px] bg-[#84754e] rounded-full cursor-pointer'></div>
-            <div className='h-[28px] w-[28px] bg-black rounded-full cursor-pointer'></div>
             </div>
+            <Button className="h-[45px] w-[200px] rounded-none bg-black text-[14px]">
+              ADICIONAR AO CARRINHO
+            </Button>
           </div>
-
-          <div className="space-y-6">
-            {product.shippingInfo && (
-              <div className="flex items-center space-x-4">
-                <div className="min-w-[45px]">
-                  <Image
-                    src={envioImg}
-                    width={45}
-                    height={45}
-                    alt="Envio gratis"
-                  />
-                </div>
-                <div>
-                  <h4 className="text-[11px] font-semibold tracking-[2px] text-lightPink">
-                    {product.shippingInfo.title}
-                  </h4>
-                  <p className="text-[11px] leading-[20px] tracking-[2px] text-gray-600">
-                    {product.shippingInfo.description}
-                  </p>
-                </div>
-              </div>
-            )}
-
-            {product.returnInfo && (
-              <div className="flex items-center space-x-4">
-                <div className="min-w-[45px]">
-                  <Image
-                    src={cambiosImg}
-                    width={45}
-                    height={45}
-                    alt="Cambios y devoluciones"
-                  />
-                </div>
-                <div>
-                  <h4 className="text-[11px] font-semibold tracking-[2px] text-lightPink">
-                    {product.returnInfo.title}
-                  </h4>
-                  <p className="text-[11px] leading-[20px] tracking-[2px] text-gray-600">
-                    {product.returnInfo.description}
-                  </p>
-                </div>
-              </div>
-            )}
-
-            {product.paymentInfo && (
-              <div className="flex items-center space-x-4">
-                <div className="min-w-[45px]">
-                  <Image
-                    src={logoTargeta}
-                    width={45}
-                    height={45}
-                    alt="Cambios y devoluciones"
-                  />
-                </div>
-                <div>
-                  <h4 className="text-[11px] font-semibold tracking-[2px] text-lightPink">
-                    {product.paymentInfo.title}
-                  </h4>
-                  <p className="text-[11px] leading-[20px] tracking-[2px] text-gray-600">
-                    {product.paymentInfo.description}
-                  </p>
-                </div>
-              </div>
-            )}
-          </div>
-        </section>
-      </div>
-
-
-      <div className="fixed bottom-0 left-0 right-0 z-10 bg-white shadow-md md:hidden">
-        <div className="flex items-center justify-between px-4 py-2">
-          <div>
-            <span className="text-[14px] text-lg tracking-[2px] text-black">${product.price}</span>
-          </div>
-          <Button className="h-[45px] w-[200px] rounded-none bg-black text-[14px]">
-            ADICIONAR AO CARRINHO
-          </Button>
         </div>
       </div>
 
-
- 
+      <div className="mt-20 hidden lg:block xl:block">
+        <div className="mb-12 flex items-center justify-center">
+          <h2 className="text-[24px] font-medium tracking-[2px]">
+            Productos Similares
+          </h2>
+        </div>
+        <div>
+          <CarouselComponent mockProducts={mockProducts} />
+        </div>
+      </div>
     </div>
-
-    <div className='mt-20 lg:block xl:block hidden'>
-    <div className='flex items-center justify-center mb-12'>
-      <h2 className='text-[24px] tracking-[2px] font-medium'>Productos Similares</h2>
-    </div>
-    <div>
-    <CarouselComponent mockProducts={mockProducts} />
-    </div>
-    </div>
-   </div>
   );
 }
--- components/show-more-products.tsx
+++ components/show-more-products.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react';
-import { ProductItem } from './product-item';
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './shadcn/ui/carousel';
 import { ProductItemCarousel } from './product-item-carousel';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from './shadcn/ui/carousel';
 
-export const CarouselComponent = ({ mockProducts }) => {
-  const itemsPerCarouselItem = 4; // Número de produtos por item de carrossel
-  const totalItems = mockProducts.length;
-
-  // Função para agrupar os produtos em blocos de 4
+export const CarouselComponent = ({ mockProducts }: any) => {
+  const itemsPerCarouselItem = 4;
   const groupProducts = () => {
-    let groups = [];
-    for (let i = 0; i < totalItems; i += itemsPerCarouselItem) {
+    const groups = [];
+    for (let i = 0; i < mockProducts.length; i += itemsPerCarouselItem) {
       groups.push(mockProducts.slice(i, i + itemsPerCarouselItem));
     }
     return groups;
@@ -19,29 +20,29 @@ export const CarouselComponent = ({ mockProducts }) => {
   const productGroups = groupProducts();
 
   return (
-    <Carousel>
-      <CarouselContent className="flex overflow-hidden space-x-4">
+    <Carousel className="relative">
+      <CarouselContent className="flex space-x-4">
         {productGroups.map((group, index) => (
-          <CarouselItem key={index} className="flex-shrink-0 w-full sm:w-1/4">
-            <div className="flex space-x-4">
+          <CarouselItem key={index} className="w-full flex-shrink-0">
+            <div className="grid grid-cols-4 gap-4">
               {group.map((product, idx) => (
-                <div key={idx} className="w-1/4">
-                  <ProductItemCarousel
-                    name={product.name}
-                    price={product.price}
-                    images={product.images}
-                    colors={product.colors}
-                  />
-                </div>
+                <ProductItemCarousel
+                  key={idx}
+                  name={product.name}
+                  price={product.price}
+                  images={product.images}
+                  colors={product.colors}
+                />
               ))}
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="absolute left-0 top-[40%] transform -translate-y-1/2 border-none shadow-none bg-transparent  text-white p-2 rounded-full h-[52px] w-[52px] hover:bg-transparent">
+
+      <CarouselPrevious className="absolute left-0 top-[50%] h-[52px] w-[52px] -translate-y-1/2 transform rounded-full bg-transparent p-2 text-white hover:bg-gray-800">
         Previous
       </CarouselPrevious>
-      <CarouselNext className="absolute right-0 top-[40%] transform -translate-y-1/2 border-none shadow-none bg-transparent  text-white  rounded-full h-[52px] w-[52px] hover:bg-transparent">
+      <CarouselNext className="absolute right-0 top-[50%] h-[52px] w-[52px] -translate-y-1/2 transform rounded-full bg-transparent p-2 text-white hover:bg-gray-800">
         Next
       </CarouselNext>
     </Carousel>
